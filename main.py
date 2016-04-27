5ebaf0cee37621d4a5bb821fd3801d6e2048cc8e
mapineda <mapineda1222@gmail.com>
2016-04-27 17:48:42 -0500

    added comments to basic black app

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
+#It’s required that the base Class of your Kivy App inherits from the App class
+#The uix module is the section that holds the user interface elements like layouts and widgets.
 from kivy.app import App
 from kivy.uix.widget import Widget
 
 class PongGame(Widget):
     pass
-
+#define base Class for kivy app
+#function initializes and returns the Root Widget
 class PongApp(App):
     def build(self):
         return PongGame()
-
+#here the class PongApp is initialized and its run() method called. This initializes and starts our Kivy application.
 if __name__ == '__main__':
     PongApp().run()
